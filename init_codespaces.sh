54f3bbb75c6dd428be6422b7cc0a3cd5d796f853
Luiz Camargos <46843342+LuizCamargos@users.noreply.github.com>
2024-06-17 14:51:26 +0000

    init_codespaces

---
+++ init_codespaces.sh
@@ -0,0 +1,19 @@
+cp .env.example .env
+
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php82-composer:latest \
+    composer install --ignore-platform-reqs
+
+./vendor/bin/sail up
+
+./vendor/bin/sail php artisan key:generate
+./vendor/bin/sail php artisan migrate:fresh --seed
+
+#instalar e rodar Vite
+#npm install
+#npm run dev
+
+#relacionado: https://laracasts.com/discuss/channels/devops/laravel-10-vite-and-codespaces
